2ecc29e73f4e46788a51ad0ff6d15cf1a125f20a
Danguilhen <thomas.danguilhen@estaca.eu>
2022-03-21 15:06:26 +0100

    lancer échéancier

--- sources/SED.cpp
+++ sources/SED.cpp
@@ -8,11 +8,24 @@ void SED::ajouter(Evenement evenement)
 
 void SED::lancer()
 {
+    int index;
+    float min;
     Evenement evenement;
+
     while (!_evenements.empty())
     {
-        evenement = _evenements.front();
-        _evenements.erase(_evenements.begin());
+        index = 0;
+        min = _evenements[0].heure();
+        for(int i=0; i<_evenements.size(); i++)
+        {
+            if(_evenements[i].heure() < min)
+            {
+                min = _evenements[i].heure();
+                index = i;
+            }
+        }
+        evenement = _evenements.atIndex(index);
+        _evenements.erase(_evenements.begin() + index);
         evenement.traiter();
     }
     _dureeReel = evenement.heure();
