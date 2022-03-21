61370f077a76ffaf684ca34429bed694eeb5969c
benmessao <celia.benmessaouda@gmail.com>
2022-03-21 13:38:42 +0100

    main ok

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,48 +1,34 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <iostream>
+#include <cstring>
+#include <vector>
+using namespace std;
 
+//g++ -o main main.cpp
 int main(int argc, char **argv)
 {
-    int aflag = 0;
-    int bflag = 0;
-    char *cvalue = NULL;
-    int index;
-    int c;
+    int dureePrevue;
+    int nombreCaissiers = 0;
+    vector<float> tempsMoyenService;
+    float tempsEntreArrivees;
+    for(int i=1; i<argc; i++)
+    {       
+        if (strcmp(argv[i], "-dp") == 0) //durée prévue
+            dureePrevue = stoi(argv[i+1]);
+        else if (strcmp(argv[i], "-nc") == 0) //nombre de caissiers
+            nombreCaissiers = stoi(argv[i+1]);
+        else if (strcmp(argv[i], "-ts") == 0) //temps de service
+            for(int j=1; j<nombreCaissiers+1; j++)
+                tempsMoyenService.push_back(stof(argv[i+j]));
+        else if (strcmp(argv[i], "-ta") == 0)//temps entre arrivée
+            tempsEntreArrivees = stof(argv[i+1]);
+    }
 
-    opterr = 0;
-
-    while ((c = getopt(argc, argv, "abc:")) != -1)
-        switch (c)
-        {
-        case 'a':
-            aflag = 1;
-            break;
-        case 'b':
-            bflag = 1;
-            break;
-        case 'c':
-            cvalue = optarg;
-            break;
-        case '?':
-            if (optopt == 'c')
-                fprintf(stderr, "Option -%c requires an argument.\n", optopt);
-            else if (isprint(optopt))
-                fprintf(stderr, "Unknown option `-%c'.\n", optopt);
-            else
-                fprintf(stderr,
-                        "Unknown option character `\\x%x'.\n",
-                        optopt);
-            return 1;
-        default:
-            abort();
-        }
-
-    printf("aflag = %d, bflag = %d, cvalue = %s\n",
-           aflag, bflag, cvalue);
-
-    for (index = optind; index < argc; index++)
-        printf("Non-option argument %s\n", argv[index]);
-    return 0;
+    cout << dureePrevue << endl;
+    cout << nombreCaissiers << endl;
+    for (int i = 0; i<nombreCaissiers; i++)
+        cout << tempsMoyenService[i] <<endl;
+    cout << tempsEntreArrivees << endl;
 }
